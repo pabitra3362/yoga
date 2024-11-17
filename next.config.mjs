5c35ba469375067297dc9c1c2b3db2851ca0fc88
pabitra3362 <pmohanty1435@gmail.com>
2024-11-17 20:37:04 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // Make sure App Router is enabled
+  },
+  webpack(config, { isServer }) {
+    // If running on server, fix app directory location
+    if (!isServer) {
+      config.resolve.modules.push(path.resolve('./src')); // This allows imports from `src`
+    }
+    return config;
+  },
     images: {
         remotePatterns: [
           {
